3f932a7b061d7ce6ad4acb5dfcf5a6e9ee224df6
numa08 <n511287@gmail.com>
2014-05-17 21:21:59 +0900

    fix twitter instance

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,10 +5,16 @@ import org.pac4j.play.scala.ScalaController
 import org.pac4j.oauth.profile.twitter.TwitterProfile
 import models.twiter.TwitterAuthConfig
 import twitter4j.auth.AccessToken
-import twitter4j.TwitterFactory
+import twitter4j.{ Paging, TwitterFactory }
 import scala.collection.JavaConversions._
 
 object Application extends ScalaController with TwitterAuthConfig {
+
+  lazy val twitter = {
+    val tw = TwitterFactory.getSingleton
+    tw.setOAuthConsumer(consumer.key, consumer.secret)
+    tw
+  }
 
   def index = Action { request =>
     val newSession = getOrCreateSessionId(request)
@@ -16,10 +22,8 @@ object Application extends ScalaController with TwitterAuthConfig {
       val tp = p.asInstanceOf[TwitterProfile]
       val ac = new AccessToken(tp.getAccessToken, tp.getAccessSecret)
 
-      val tw = TwitterFactory.getSingleton
-      tw.setOAuthConsumer(consumer.key, consumer.secret)
-      tw.setOAuthAccessToken(ac)
-      tw.getUserTimeline.map(_.getText).mkString("\n")
+      twitter.setOAuthAccessToken(ac)
+      twitter.getUserTimeline(new Paging(999)).map(_.getText).mkString("\n")
     }
     Ok(content)
   }
